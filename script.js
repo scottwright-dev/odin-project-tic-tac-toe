6417ad5c438826b9761b438e2611e455de364aae
Scott Wright <scott.wright.uk@gmail.com>
2023-05-25 22:45:59 +0100

    feat: Implement basic console game functionality
    > - Added the `isMoveValid` function to the `gameBoard` module. This function checks if a move is valid by verifying if the specified cell on the board is empty.
    > - Modified the `getPlayerMove` function in the `createPlayer` module to parse the player's move input and return the row and column as an array.
    > - Implemented the `playRound` function in the `gameController` module. It prompts the current player for their move, checks if the move is valid using `isMoveValid` from `gameBoard`, and updates the board if the move is valid. If the move is invalid, it displays an error message. The current state of the board is logged to the console after each move.

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 // GAME BOARD
 const gameBoard = (() => {
     const currentBoard = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
     ];
-    
-    return { currentBoard } ;
-})();
-
-// PLAYER
-const createPlayer = (name, marker) => {
+  
+    const isMoveValid = (row, col) => {
+      if (currentBoard[row][col] === '') {
+        return true;
+      }
+      return false;
+    };
+  
+    return { currentBoard, isMoveValid };
+  })();
+  
+  // PLAYER
+  const createPlayer = (name, marker) => {
     const getPlayerMove = () => {
-        const playerMove = prompt(`${name}, enter your move:`);
-      return playerMove;
+      const playerMove = prompt(`${name}, enter your move:`);
+      const [row, col] = playerMove.split(',').map(Number);
+      return [row, col];
     };
   
     return { name, marker, getPlayerMove };
@@ -21,13 +29,32 @@ const createPlayer = (name, marker) => {
   
   const player1 = createPlayer('Player 1', 'O');
   const player2 = createPlayer('Player 2', 'X');
-
-// GAME CONTROLLER
-const gameController = (() => {
- 
-})();
-
-// TESTING LOGS
-
-console.log(gameController);
-
+  
+  // GAME CONTROLLER
+  const gameController = (() => {
+    const playRound = () => {
+      const players = [player1, player2];
+      let currentPlayer = players[0];
+  
+      const playerMove = currentPlayer.getPlayerMove();
+      const [row, col] = playerMove;
+      const isValidMove = gameBoard.isMoveValid(row, col);
+  
+      if (isValidMove) {
+        gameBoard.currentBoard[row][col] = currentPlayer.marker;
+      } else {
+        console.log('Invalid move. Try again.');
+      }
+  
+      console.log(gameBoard.currentBoard);
+  
+      currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+  
+    return { playRound };
+  })();
+  
+  // TESTING LOGS
+  
+  gameController.playRound();
+  
