4233558a28905720cfde884076dfd263413082b0
Scott Wright <scott.wright.uk@gmail.com>
2023-05-26 15:46:54 +0100

    feat: add checkForWin method to gameController
    The checkForWin function checks the current game board for winning combinations.
    It compares the markers of the current player with predefined winning combinations.
    If a winning combination is found, it returns true; otherwise returns false.
    
    * added some todo comments to flag eslint warnings. I will address these later.

--- script.js
+++ script.js
@@ -48,18 +48,48 @@ const gameController = (() => {
   
       if (isValidMove) {
         console.log('Valid move. Updating the board.');
+        console.log('Current Board:', gameBoard.getCurrentBoard());
+        // TODO: The checkForWin variable is used before declaration. Need to refactor.
+        if (checkForWin()) {
+          console.log(`${currentPlayer.name} wins! 3 in a row!`);
+          return;
+        }
         switchPlayer();
+        playRound();
       } else {
         console.log('Invalid move. Try again.');
         playRound();
       }
-  
-      console.log('Current Board:', gameBoard.getCurrentBoard());
-  
-      playRound();
+    };
+
+    const checkForWin = () => {
+      const board = gameBoard.getCurrentBoard();
+    
+      const winningCombinations = [
+        // Rows
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+    
+        // Columns
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+    
+        // Diagonals
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+      ];
+    //TODO: This is being flagged by eslint: "loops should be avoided in favor of array iterations"
+      for (const combination of winningCombinations) {
+        if (combination.every((square) => square === currentPlayer.marker)) {
+          return true;
+        }
+      }
+      return false;
     };
   
-    return { playRound };
+    return { playRound, checkForWin };
   })();
   
   // TESTING LOGS
